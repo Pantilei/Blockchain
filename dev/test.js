89e9e8d92f0e2d51fc94ae6b59423eefd4bbc3ee
pantilei <ianulov.pantilei@gmail.com>
2021-05-06 22:24:53 +0300

    Create blockchain datastructure

---
+++ dev/test.js
@@ -0,0 +1,22 @@
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+
+const previousHBlockash = ";LSKDHFG;LAKJSD;FLKJ";
+
+const currentBlockData = [{
+    amount: 100,
+    sender: "LHADSF;LGAKJS;DLFK",
+    recipient: "LAHSKFG;LSKFD"
+}, {
+    amount: 10,
+    sender: "LHADSFLGAKJS;DLFK",
+    recipient: "LAHSKFG;LSKFD"
+}, {
+    amount: 20,
+    sender: "LHADSF;LGAKJS;DLFK",
+    recipient: "LAHSKFG;LSKFD"
+}];
+
+console.log(bitcoin);
